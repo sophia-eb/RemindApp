57935a4c9524e283e6fa8eb6fb97f61c77358af4
Sophia Liu <liushanshan@easternbay.cn>
2021-12-23 11:37:09 +0800

    Add the weather list

---
+++ screens/WeatherList/CustomChart.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { VictoryChart, VictoryLine, VictoryScatter, VictoryTheme } from "victory-native";
+
+
+const CustomChart = props => {
+  const { data } = props;
+  return (
+    <VictoryChart
+      width={380}
+      height={200}
+      domainPadding={{x: 10, y: 6}}
+      >
+      {data[0]?.temp &&
+        <VictoryLine
+          data={data} x="hour" y="temp"
+          style={{ data: { stroke: "#4fd6f7", strokeWidth: 2, strokeLinecap: "round" } }}
+        />
+      }
+      {data[0]?.temp &&
+        <VictoryScatter
+          data={data}
+          x="hour"
+          y="temp"
+          style={{ data: { fill: "#0583d2" } }}
+          size={3}
+        />
+      }
+      {data[0]?.maxTemp &&
+        <VictoryLine
+          data={data} x="hour" y="maxTemp"
+          style={{ data: { stroke: "#0583d2", strokeWidth: 2, strokeLinecap: "round" } }}
+        />
+      }
+      {data[0]?.maxTemp &&
+        <VictoryScatter
+          data={data}
+          x="hour"
+          y="maxTemp"
+          style={{ data: { fill: "#0583d2" } }}
+          size={3}
+        />
+      }
+      {data[0]?.minTemp &&
+        <VictoryLine
+          data={data} x="hour" y="minTemp"
+          style={{ data: { stroke: "#4fd6f7", strokeWidth: 2, strokeLinecap: "round" } }}
+        />
+      }
+      {data[0]?.minTemp &&
+        <VictoryScatter
+          data={data}
+          x="hour"
+          y="minTemp"
+          style={{ data: { fill: "#4fd6f7" } }}
+          size={3}
+        />
+      }
+    </VictoryChart>
+  );
+};
+
+export default CustomChart;
---
+++ utils/apiUtils.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const nowWeatherUrl = "https://devapi.qweather.com/v7/weather/now?";
+const publicId = "HE2112211554191382";
+const key = "db11be461e024ddf890b8cf02baa548c";
+// const location = "101010100"; //beijing
+const location = "101190401"; //suzhou
+
+export async function getNowWeather() {
+  const url = nowWeatherUrl + "key=" + key + "&location=" + location;
+  return axios({
+    method: "get",
+    url: url,
+  });
+}
+
+const weatherUrl24h = "https://devapi.qweather.com/v7/weather/24h?";
+
+export async function get24hWeather() {
+  const location = "101190401"; //suzhou
+  const url = weatherUrl24h + "key=" + key + "&location=" + location;
+
+  return axios({
+    method: "get",
+    url: url,
+  });
+}
+
+const weatherUrl3day = "https://devapi.qweather.com/v7/weather/3d?";
+const weatherUrl7day = "https://devapi.qweather.com/v7/weather/7d?";
+
+export async function get7daysWeather() {
+  const location = "101190401"; //suzhou
+  const url = weatherUrl24h + "key=" + key + "&location=" + location;
+
+  return axios({
+    method: "get",
+    url: url,
+  });
+}
